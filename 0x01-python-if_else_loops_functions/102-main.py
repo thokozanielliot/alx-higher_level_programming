dde33ef56a8a170e4205f99af9bfdd0606affdd7
Thokozani Elliot <thokozanielliot58@gmail.com>
2023-08-09 09:26:46 +0200

    Added files and Updated files

---
+++ 0x01-python-if_else_loops_functions/102-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(9, 4, 7))
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3:
-            print("{}".format("Fizz"), end=" ")
-        elif i % 5:
-            print("{}".format("Buzz"), end=" ")
-        elif i % 5 and i % 3:
+        if i % 3 == 0 and i % 5 == 0:
             print("{}".format("FizzBuzz"))
+        elif i % 3 == 0:
+            print("{}". format("Fizz"))
+        elif i % 5 == 0:
+            print("{}".format("Buzz"))
         else:
             print("{}".format(i))
