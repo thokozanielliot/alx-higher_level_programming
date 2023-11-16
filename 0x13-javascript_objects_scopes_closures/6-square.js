28effedf75c14f3f3d4ddf77eefdee4340a8e2b4
thokozanielliot <thokozanielliot58@gmail.com>
2023-11-16 06:28:18 -0800

    Fix: Semistandard

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
