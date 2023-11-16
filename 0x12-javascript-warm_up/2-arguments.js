ebde1596e2b91e89f01c9beadf73ece3f6eecfc5
Thokozani Elliot <thokozanielliot58@gmail.com>
2023-11-16 21:10:33 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
-  console.log('No arguement');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Arguement found');
+  console.log('Argument found');
 } else {
-  console.log('Arguements found');
+  console.log('Arguments found');
 }
